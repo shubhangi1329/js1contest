52485ecfd02c81cba27c9f18178e44531dbfc87d
shubhangi1329 <13shubhangi.saraswat@gmail.com>
2023-01-07 23:20:50 +0530

    final

--- script.js
+++ script.js
@@ -7,21 +7,40 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
+ arr.map(print);
+ function print(arritem){
+  if(arritem.profession==="developer"){
+    console.log(arritem);
+  }
+ }
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here
+  arr.forEach(print2);
+  function print2(arritem){
+    if(arritem.profession=== "developer"){
+      console.log(arritem);
+    }
+  }
 }
 
 function addData() {
-  //Write your code here
+  let obj={id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(obj);
+  console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here
+     let spliced=arr.splice(2,1);
+     console.log(arr);
 }
 
 function concatenateArray() {
-  //Write your code here
+  let arr2=[
+    {id:5,name:"kumud",age:"29",profession:"Singer"},
+    {id:6,name:"shubhi",age:"24",profession:"Developer"},
+    {id:7,name:"sristhi",age:"24",profession:"finance"},
+  ];
+  let concat=arr.concat(arr2);
+  console.log(concat);
 }
